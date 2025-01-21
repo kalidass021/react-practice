da1b926655450cb87eeda8c77e367b15a3aea129
Kalidass <kalidass.k021@gmail.com>
2025-01-21 12:29:06 +0530

    develop filter top rated restaurants functionality

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,12 +1,26 @@
+import { useState } from 'react';
 import RestaurantCard from './RestaurantCard';
 import { resList } from '../utils/mockData';
 
 const Body = () => {
+  const [restaurants, setRestaurants] = useState(resList);
+  const handleClick = () => {
+      const topRatedRestaurants = resList.filter(({info}) => info.avgRating > 4.5);
+      setRestaurants(topRatedRestaurants);
+  }
   return (
     <div className='body'>
       <div className='search'>Search</div>
+      <div className='filter'>
+        <button
+          className='filter-btn'
+          onClick={handleClick}
+        >
+          Top Rated
+        </button>
+      </div>
       <div className='res-container'>
-        {resList.map((restaurant) => (
+        {restaurants.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
