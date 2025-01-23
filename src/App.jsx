deac75c0fdd4279710ab1e00fc7e476633df5dce
Kalidass <kalidass.k021@gmail.com>
2025-01-23 21:25:50 +0530

    install and config react-router v7

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,45 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Header from './components/Header';
 import Body from './components/Body';
+import About from './components/About';
+import Contact from './components/Contact';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router';
+
 
 const App = () => {
   return (
     <div className='app'>
       <Header />
-      <Body />
+      <Outlet />
     </div>
   );
 };
 
+
+const appRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Body />,
+      },
+      {
+        path: '/about',
+        element: <About />,
+      },
+      {
+        path: '/contact',
+        element: <Contact />,
+      }
+    ]
+  },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <RouterProvider router={appRouter}>
+    <App />
+  </RouterProvider>
+);
