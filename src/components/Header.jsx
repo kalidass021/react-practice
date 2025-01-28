d40275f6116b386424b00cef7d22daa823216b9c
Kalidass <kalidass.k021@gmail.com>
2025-01-28 13:30:12 +0530

    style header using tailwind css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,24 +5,24 @@ import { Link } from 'react-router';
 const Header = () => {
   const [btnName, setBtnName] = useState('login');
   return (
-    <div className='header'>
+    <div className='flex justify-between bg-pink-200 shadow-lg border border-solid border-black'>
     {console.log('header rendered')}
       <div className='logo-container'>
-        <img className='logo' src={LOGO_URL} alt='logo' width={100} />
+        <img className='w-20' src={LOGO_URL} alt='logo' width={100} />
       </div>
-      <div className='nav-items'>
-        <ul>
-          <li>
+      <div className='flex items-center'>
+        <ul className='flex p-4 m-4'>
+          <li className='mx-2'>
             <Link to="/">Home</Link>
           </li>
-          <li>
+          <li className='mx-2'>
             <Link to="/about">About</Link>
           </li>
-          <li>
+          <li className='mx-2'>
             <Link to="/contact">Contact</Link>
           </li>
-          <li>Cart</li>
-          <li>
+          <li className='mx-2'>Cart</li>
+          <li className='mx-2'>
             <Link to='/grocery'>Grocery</Link>
           </li>
           <button className='login' onClick={() => {
